208849d2b487ca4817152ad744b42563d60cce90
Erbert Rotondano <erotondano@senhasegura.com>
2024-10-31 15:52:48 -0300

    feat: add layout md

---
+++ src/images/Footer/icons.js
@@ -0,0 +1,53 @@
+import Alura from './Alura.svg'
+import AluraEmpresas from './AluraEmpresas.svg'
+import AluraLATAM from './AluraLATAM.svg'
+import AluraLingua from './AluraLingua.svg'
+import AluraStart from './AluraStart.svg'
+import Caelum from './Caelum.svg'
+import CarreiraSemFronteira from './CarreiraSemFronteira.svg'
+import CasaDoCodigo from './CasaDoCodigo.svg'
+import GUJ from './GUJ.svg'
+import HipstersJobs from './HipstersJobs.svg'
+import HipstersTech from './HipstersTech.svg'
+import LayersTech from './LayersTech.svg'
+import LikeABoss from './LikeABoss.svg'
+import MusicDot from './MusicDot.svg'
+import PM3 from './PM3.svg'
+import ScubaDev from './ScubaDev.svg'
+
+const footerIcons = [
+    {
+        'col_title': 'Educação',
+        'items': [
+            {'name': 'CasaDoCodigo', 'logo': CasaDoCodigo},
+            {'name': 'Caelum', 'logo': Caelum},
+        ],
+    },
+    {
+        'col_title': 'Educação Online',
+        'items': [
+            {'name': 'Alura', 'logo': Alura},
+            {'name': 'AluraEmpresas', 'logo': AluraEmpresas},
+            {'name': 'AluraLATAM', 'logo': AluraLATAM},
+            {'name': 'AluraLingua', 'logo': AluraLingua},
+            {'name': 'AluraStart', 'logo': AluraStart},
+            {'name': 'MusicDot', 'logo': MusicDot},
+            {'name': 'PM3', 'logo': PM3},
+        ]
+    },
+    {
+        'col_title': 'Comunidade',
+        'items': [
+            {'name': 'CarreiraSemFronteira', 'logo': CarreiraSemFronteira},
+            {'name': 'GUJ', 'logo': GUJ},
+            {'name': 'HipstersJobs', 'logo': HipstersJobs},
+            {'name': 'HipstersTech', 'logo': HipstersTech},
+            {'name': 'LayersTech', 'logo': LayersTech}, 
+            {'name': 'LikeABoss', 'logo': LikeABoss},
+            {'name': 'ScubaDev', 'logo': ScubaDev},
+        ]
+    }
+     
+];
+
+export default footerIcons;
