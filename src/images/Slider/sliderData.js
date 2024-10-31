7b4ab9048a1a00b92c6b996fa385914969f0e95c
Erbert Rotondano <erotondano@senhasegura.com>
2024-10-31 09:51:46 -0300

    feat: primeiros elementos

---
+++ src/images/Slider/sliderData.js
@@ -0,0 +1,17 @@
+import gestao2 from './Gestão2.svg';
+import nodeJs from './Nodejs.svg';
+import tuning from './Tuning.svg';
+import arquitetura from './Arquitetura.svg';
+import construct2 from './Construct2.svg';
+import gestao from './Gestão.svg';
+
+const sliderItems = [
+    gestao2,
+    nodeJs,
+    tuning,
+    arquitetura,
+    construct2,
+    gestao,
+];
+
+export default sliderItems;
---
+++ src/images/icons.js
@@ -0,0 +1,16 @@
+import favoritos from './Menu/Favoritos.svg';
+import logo from './Menu/Logo.svg';
+import menu from './Menu/Menu.svg';
+import sacola from './Menu/Sacola.svg';
+import usuario from './Menu/Usuário.svg';
+
+
+const icons = {
+    favoritos,
+    logo,
+    menu,
+    sacola,
+    usuario,
+}
+
+export default icons;
